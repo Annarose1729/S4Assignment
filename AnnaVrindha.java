1b4cd17a263b5d14c733463ae262593e85bc901b
Annarose1729 <35303861+Annarose1729@users.noreply.github.com>
2019-05-07 06:47:35 +0530

    Add files via upload

---
+++ AnnaVrindha.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Oodassignment
+{
+ public static double valueofe()
+  {
+   
+    double e =2.71828;
+     return e;
+  
+  }
+
+ public static double valueofpi()
+  {
+   double pi = 3.14159;
+   return(pi);
+  }
+}
